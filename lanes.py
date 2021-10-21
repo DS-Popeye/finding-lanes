fbbd2289b5ccfaa79ac04ab606b8a3df696519cd
Shuvo-70 <rabiul35-487@diu.edu.bd>
2021-10-21 20:44:22 +0600

    This i opencv project

---
+++ lanes.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+def canny(image):
+    gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+    blur = cv2.GaussianBlur(gray,(5,5),0)
+    canny = cv2.Canny(blur,50,150)
+    return canny
+
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+    [(200,height),(1100, height), (550,250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons , 255)
+    return mask
+
+image =cv2.imread('test_image.jpg')
+lane_image = np.copy(image)
+canny = canny(lane_image)
+cv2.imshow("result",region_of_interest(canny))
+cv2.waitKey(0)
