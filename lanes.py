432e0c9bb3535efb484be72b4942886fd91906e6
Shuvo-70 <rabiul35-487@diu.edu.bd>
2021-10-22 10:27:58 +0600

    Now i am update my work.

--- lanes.py
+++ lanes.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 
+
 def canny(image):
     gray = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
     blur = cv2.GaussianBlur(gray,(5,5),0)
@@ -14,10 +15,12 @@ def region_of_interest(image):
     ])
     mask = np.zeros_like(image)
     cv2.fillPoly(mask, polygons , 255)
-    return mask
+    masked_image = cv2.bitwise_and(image, mask)#
+    return masked_image
 
 image =cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
-cv2.imshow("result",region_of_interest(canny))
+croped_image = region_of_interest(canny)
+cv2.imshow("result",croped_image)
 cv2.waitKey(0)
